caeaa0cfc4db948c619e951c311d5658ab92a14a
515hikaru <karanodeny@gmail.com>
2018-01-27 14:16:59 +0900

    test edit

--- tests/test_make2help.py
+++ tests/test_make2help.py
@@ -1,12 +1,14 @@
 """
 Unit Test for make2help.py
 """
-import os
+import pathlib
 import unittest
 
 import make2help
 
-BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+BASE_DIR = pathlib.Path(__file__).absolute().parent.parent / pathlib.Path(
+    'sample')
+
 
 class TestPrintString(unittest.TestCase):
     """出力文字列が想定通りかをテスト"""
@@ -21,9 +23,22 @@ class TestPrintString(unittest.TestCase):
         という形式のMakefileのテスト
         出力されるヘルプは 'bar:\tfoo' となる
         """
-        makefile_path = os.path.join(BASE_DIR, 'sample/Makefile1')
+        makefile_path = BASE_DIR / pathlib.Path('Makefile1')
         result = make2help.format_makehelp(makefile_path)
-        expected = ('bar:\tfoo')
+        expected = 'bar:\tfoo'
+        self.assertEqual(next(result), expected)
+
+    def test_comment_empty_target(self):
+        """
+        ```
+        bar:
+        	@echo "foo"
+        ```
+        のように `##` がなかった場合でも `bar` は出力をする
+        """
+        makefile_path = BASE_DIR / pathlib.Path('Makefile2')
+        result = make2help.format_makehelp(makefile_path)
+        expected = 'bar'
         self.assertEqual(next(result), expected)
 
 
